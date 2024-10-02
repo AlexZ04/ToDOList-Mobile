94da45b2623c53751a5aa09cad62988b52b9482c
Aleksey Zinovjev <alexz04tab2@gmail.com>
2024-10-02 23:21:48 +0700

    add api connection

--- app/src/main/java/com/example/todolist_mobile2/MainActivity.kt
+++ app/src/main/java/com/example/todolist_mobile2/MainActivity.kt
@@ -1,8 +1,9 @@
 package com.example.todolist_mobile2
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Bundle
-import android.widget.Toast
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -20,7 +21,6 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
@@ -28,7 +28,6 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.CheckboxDefaults
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -39,7 +38,6 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color.Companion.Black
@@ -47,18 +45,39 @@ import androidx.compose.ui.graphics.Color.Companion.Gray
 import androidx.compose.ui.graphics.Color.Companion.Green
 import androidx.compose.ui.graphics.Color.Companion.LightGray
 import androidx.compose.ui.graphics.Color.Companion.White
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.todolist_mobile2.ui.theme.ToDoListMobile2Theme
-import com.google.gson.Gson
 import com.google.gson.annotations.SerializedName
-import com.google.gson.reflect.TypeToken
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.PATCH
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
 import java.io.File
 
+val BASE_URL = "https://59ce-79-136-243-76.ngrok-free.app"
+
+val retrofit = Retrofit.Builder()
+    .baseUrl(BASE_URL)
+    .addConverterFactory(GsonConverterFactory.create())
+    .build()
+
+val messagesApi = retrofit.create(TaskApi::class.java)
+
+var editingTask = 0
+var editingText = ""
+var editingComp = false
 
 class MainActivity : ComponentActivity() {
 
@@ -66,6 +85,9 @@ class MainActivity : ComponentActivity() {
         var tasksAmount = 0
     }
 
+    private val tasks = mutableStateListOf<Task>()
+
+    @SuppressLint("CoroutineCreationDuringComposition")
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
@@ -76,19 +98,39 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                 ) {
-                    val tasks = remember {
-                        mutableStateListOf(
-                            Task("aa", false, false, 0),
-                        )
-                    }
 
-                    tasks.clear()
-                    tasks.addAll(load(this))
+                    fetchTasks()
 
                     Screen(tasks, this)
                 }
             }
         }
+
+    }
+
+    fun fetchTasks() {
+        val call = messagesApi.fetchData()
+
+        call.enqueue(object : Callback<List<Note>> {
+            override fun onResponse(call: Call<List<Note>>, response: Response<List<Note>>) {
+                if (response.isSuccessful) {
+                    val data = response.body() ?: listOf()
+
+                    tasks.clear()
+                    for (i in data.indices) {
+                        tasks.add(Task(data[i].text, data[i].isCompleted, data[i].id))
+                    }
+
+                } else {
+                    Log.d("MyLog", "Request failed with code: ${response.code()}")
+                }
+            }
+
+            override fun onFailure(call: Call<List<Note>>, t: Throwable) {
+                Log.d("MyLog", "Network request failed", t)
+            }
+
+        })
 
     }
 
@@ -98,13 +140,30 @@ class MainActivity : ComponentActivity() {
 data class Task(
     @SerializedName("text") var text: String,
     @SerializedName("isCompleted") var isCompleted: Boolean,
-    @SerializedName("isEditable") var isEditable: Boolean,
     @SerializedName("number") var number: Number
 )
 
+data class Note(
+    var id: Number,
+    var text: String,
+    var isCompleted: Boolean
+)
+
+interface TaskApi {
+    @GET("Tasks")
+    fun fetchData(): Call<List<Note>>
+    @POST("Tasks")
+    fun createTask(@Body newTask: Note): Call<Note>
+    @DELETE("Tasks/{id}")
+    fun deleteTask(@Path("id") id: Int): Call<Void>
+    @PATCH("Tasks/{id}")
+    fun checkTask(@Path("id") id: Int): Call<Void>
+    @PUT("Tasks/{id}")
+    fun changeTask(@Path("id") id: Number, @Body newTask: Note): Call<Void>
+}
+
 @Composable
 fun Screen(tasks: MutableList<Task>, context: Context) {
-
     val focusManager = LocalFocusManager.current
 
     Column(
@@ -155,15 +214,38 @@ fun Screen(tasks: MutableList<Task>, context: Context) {
 
             Button(
                 onClick = {
-                    tasks.add(
-                        Task(
-                            "Дело", false, true,
-                            MainActivity.tasksAmount
-                        )
-                    )
 
-                    MainActivity.tasksAmount++
                     focusManager.clearFocus()
+                    changeTaskText()
+
+                    val call =
+                        messagesApi.createTask(Note(MainActivity.tasksAmount - 1, "Дело",
+                            false))
+                    call.enqueue(object : Callback<Note> {
+                        override fun onResponse(call: Call<Note>, response: Response<Note>) {
+                            if (!response.isSuccessful) {
+                                Log.d("MyLog", "Error with creating task: ${response.code()}")
+                            }
+                            else {
+                                val data = response.body()
+
+                                if (data != null) {
+                                    tasks.add(
+                                        Task(
+                                            "Дело", false,
+                                            data.id
+                                        )
+                                    )
+                                }
+
+                                MainActivity.tasksAmount++
+                            }
+                        }
+
+                        override fun onFailure(call: Call<Note>, t: Throwable) {
+                            Log.d("MyLog", "Error with creating task", t)
+                        }
+                    })
                 },
                 shape = RoundedCornerShape(15.dp),
                 colors = ButtonDefaults.buttonColors(
@@ -174,73 +256,13 @@ fun Screen(tasks: MutableList<Task>, context: Context) {
                 Text(text = "Добавить")
             }
 
-            SaveButton(tasks)
-
-            Button(
-                onClick = {
-                    tasks.clear()
-                    tasks.addAll(load(context))
-                    focusManager.clearFocus()
-                },
-                shape = RoundedCornerShape(15.dp),
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = Black
-                ),
-                modifier = Modifier.width(120.dp)
-            ) {
-                Text(text = "Загрузить")
-            }
         }
 
     }
 }
 
-
-@Composable
-fun SaveButton(tasks: MutableList<Task>) {
-    val context = LocalContext.current
-
-    val focusManager = LocalFocusManager.current
-
-    Button(
-        onClick = {
-            save(context, tasks)
-            focusManager.clearFocus()
-        },
-        shape = RoundedCornerShape(15.dp),
-        colors = ButtonDefaults.buttonColors(
-            containerColor = Black
-        ),
-        modifier = Modifier.width(120.dp)
-    ) {
-        Text(text = "Сохранить")
-    }
-}
-
-fun save(context: Context, tasks: MutableList<Task>) {
-    Toast.makeText(context, "Сохранено!", Toast.LENGTH_SHORT).show()
-
-    val gson = Gson()
-    val json = gson.toJson(tasks)
-
-    val file = File(context.filesDir, "tasks.json")
-    file.writeText(json)
-}
-
-fun load(context: Context): List<Task> {
-    val gson = Gson()
-    val file = File(context.filesDir, "tasks.json")
-
-    return if (file.exists()) {
-        gson.fromJson(file.readText(), object : TypeToken<List<Task>>() {}.type)
-    } else {
-        emptyList()
-    }
-}
-
 @Composable
 fun CreateTask(task: Task, tasks: MutableList<Task>) {
-
     var checked by remember { mutableStateOf(task.isCompleted) }
     var text by remember { mutableStateOf(task.text) }
 
@@ -273,12 +295,16 @@ fun CreateTask(task: Task, tasks: MutableList<Task>) {
                             break
                         }
                     }
+                    editingTask = task.number.toInt()
+                    editingText = it
+                    editingComp = task.isCompleted
                 },
                 keyboardOptions = KeyboardOptions.Default.copy(
                     imeAction = ImeAction.Done
                 ),
                 keyboardActions = KeyboardActions(onDone = {
                     focusManager.clearFocus()
+                    changeTaskText()
                 }),
                 colors = OutlinedTextFieldDefaults.colors(
                     unfocusedContainerColor = Green,
@@ -297,6 +323,9 @@ fun CreateTask(task: Task, tasks: MutableList<Task>) {
                     .padding(5.dp),
                 value = text,
                 onValueChange = {
+                    editingTask = task.number.toInt()
+                    editingText = it
+                    editingComp = task.isCompleted
                     text = it
                     for (i in 0 until tasks.size) {
                         if (task.number == tasks[i].number) {
@@ -304,12 +333,14 @@ fun CreateTask(task: Task, tasks: MutableList<Task>) {
                             break
                         }
                     }
+
                 },
                 keyboardOptions = KeyboardOptions.Default.copy(
                     imeAction = ImeAction.Done
                 ),
                 keyboardActions = KeyboardActions(onDone = {
                     focusManager.clearFocus()
+                    changeTaskText()
                 }),
                 colors = OutlinedTextFieldDefaults.colors(
                     unfocusedContainerColor = White,
@@ -323,8 +354,22 @@ fun CreateTask(task: Task, tasks: MutableList<Task>) {
 
         Image(modifier = Modifier
             .clickable {
-                tasks.remove(task)
                 focusManager.clearFocus()
+
+                val call =
+                    messagesApi.deleteTask(task.number.toInt())
+                call.enqueue(object : Callback<Void> {
+                    override fun onResponse(call: Call<Void>, response: Response<Void>) {
+                        if (response.isSuccessful) {
+                            tasks.remove(task)
+                        }
+                    }
+
+                    override fun onFailure(call: Call<Void>, t: Throwable) {
+                        Log.e("MainActivity", "Failed to delete task", t)
+                        t.printStackTrace()
+                    }
+                })
             }
             .size(50.dp),
             painter = painterResource(id = R.drawable.delete),
@@ -334,13 +379,28 @@ fun CreateTask(task: Task, tasks: MutableList<Task>) {
             checked = checked,
             onCheckedChange = {
                 focusManager.clearFocus()
-                checked = it
-                for (i in 0 until tasks.size) {
-                    if (task.number == tasks[i].number) {
-                        tasks[i].isCompleted = it
-                        break
+                changeTaskText()
+
+                val call =
+                    messagesApi.checkTask(task.number.toInt())
+                call.enqueue(object : Callback<Void> {
+                    override fun onResponse(call: Call<Void>, response: Response<Void>) {
+                        if (response.isSuccessful) {
+                            checked = it
+                            for (i in 0 until tasks.size) {
+                                if (task.number == tasks[i].number) {
+                                    tasks[i].isCompleted = it
+                                    break
+                                }
+                            }
+                        }
                     }
-                }
+
+                    override fun onFailure(call: Call<Void>, t: Throwable) {
+                        Log.e("MainActivity", "Failed to rename task", t)
+                        t.printStackTrace()
+                    }
+                })
 
             },
             colors = CheckboxDefaults.colors(
@@ -351,4 +411,21 @@ fun CreateTask(task: Task, tasks: MutableList<Task>) {
 
     }
 
+}
+
+fun changeTaskText() {
+    val call =
+        messagesApi.changeTask(editingTask,
+            Note(editingTask, editingText, editingComp))
+    call.enqueue(object : Callback<Void> {
+        override fun onResponse(call: Call<Void>, response: Response<Void>) {
+            if (!response.isSuccessful) {
+                Log.d("MyLog", "Error with editing task: ${response.code()}")
+            }
+        }
+
+        override fun onFailure(call: Call<Void>, t: Throwable) {
+            Log.d("MyLog", "Ошибка сети при создании задачи", t)
+        }
+    })
 }
